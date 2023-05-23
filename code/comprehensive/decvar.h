16bf8ec45c7bfd52b889d7c9f2377dc859884217
Joy Song <jsong97224@gmail.com>
2023-05-23 13:25:53 -0600

    supp materials

---
+++ code/comprehensive/decvar.h
@@ -0,0 +1,15 @@
+#ifndef _DECVAR_H_
+#define _DECVAR_H_
+
+#define frontflg front_indicator 
+#define updateflg time
+#define attachflg attach
+#define totalcad upsize
+#define totalint upsizeint
+
+static const double pi = 3.14159265358979323846264338327950288419716939937510;
+
+
+
+
+#endif
